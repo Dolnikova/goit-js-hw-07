24e80f30b390e0466145a9b6fbd7c2b009b301f9
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-08-10 15:41:34 +0300

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,30 +2,49 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-
-import "simplelightbox/dist/simple-lightbox.min.css";
 const gallery = document.querySelector(".gallery");
-const items = [];
+const galleryItem = galleryItems
+  .map(({ preview, original, description }, index) => {
+    //
+    return `
+  <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      data-index="${index}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+// const items = []; второй вариант тоже рабочий
 
-galleryItems.forEach((element) => {
-  const galleryItem = document.createElement("div");
-  galleryItem.className = "gallery__item";
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
-  galleryLink.href = element.original;
-  const galleryImage = document.createElement("img");
-  galleryImage.className = "gallery__image";
-  galleryImage.src = element.preview;
-  galleryImage.setAttribute("data-source", element.original);
-  galleryImage.alt = element.description;
+// galleryItems.forEach((element) => {
+//   const galleryItem = document.createElement("div");
+//   galleryItem.className = "gallery__item";
+//   const galleryLink = document.createElement("a");
+//   galleryLink.className = "gallery__link";
+//   galleryLink.href = element.original;
+//   const galleryImage = document.createElement("img");
+//   galleryImage.className = "gallery__image";
+//   galleryImage.src = element.preview;
+//   galleryImage.setAttribute("data-source", element.original);
+//   galleryImage.alt = element.description;
 
-  galleryItem.append(galleryLink);
-  galleryLink.append(galleryImage);
-  items.push(galleryItem);
-});
+//   galleryItem.append(galleryLink);
+//   galleryLink.append(galleryImage);
+//   items.push(galleryItem);
+// });
 
-gallery.append(...items);
+// gallery.append(...items);
 
 gallery.addEventListener("click", (e) => {
   e.preventDefault();
